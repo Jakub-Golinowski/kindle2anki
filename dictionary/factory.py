0b8b34dd2b9c28aed44c5429a2d106fe53c93ff8
Yuhao Wu <wyhfrank@gmail.com>
2017-12-24 14:15:46 +0900

    Add online dictionary: factory and hjdict.

---
+++ dictionary/factory.py
@@ -0,0 +1,34 @@
+import importlib
+import logging
+
+
+def create_dict_class(module_class):
+    """
+    Return the dictionary class 
+    :param module_class: the name of module and class in the form of "module.class"
+    :return: class object of the dictionary
+    """
+
+    class_ = None
+    module_name, class_name = module_class.rsplit(".", 1)
+    try:
+        module_ = importlib.import_module(module_name)
+        try:
+            class_ = getattr(module_, class_name)
+        except AttributeError:
+            logging.error('Class does not exist: {}'.format(class_name))
+    except ImportError:
+        logging.error('Module does not exist: {}'.format(module_name))
+    return class_
+
+
+def test():
+    HJDict = create_dict_class("hjdict.HJDict")
+    if HJDict is not None:
+        my_dict = HJDict()
+        res = my_dict.look_up("好き")
+        print(res)
+
+
+if __name__ == '__main__':
+    test()
