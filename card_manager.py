03cc1976bb158e996838d3869e393fdf81ad60c2
Yuhao Wu <wyhfrank@gmail.com>
2018-01-04 21:39:07 +0900

    Add tags for notes.

--- card_manager.py
+++ card_manager.py
@@ -14,44 +14,8 @@ class CardManager(object):
         self.deckID = self.deck.decks.id(deck_name)
         self.deck.decks.select(self.deckID)
 
-    def create(self, card_front, card_back):
-        logging.info("Get Collection/Deck '" + self.coll_file + "/" + self.deck_name +
-              "'")
-        # deck = aopen(self.coll_file)
-        # deckId = deck.decks.id(self.deck_name)
 
-        # deck.decks.select(deckId)
-        basic_model = self.deck.models.byName('Basic')
-        basic_model['did'] = self.deckID
-
-        # todo I don't see any other ways to prevent creating a new Deck
-        if self.deck.cardCount == 0:
-            sys.exit("ERROR: Collection/Deck '" + coll_file + "/" + deck_name +
-                     "' does not exist.")
-
-        logging.info("Deck has " + str(self.deck.cardCount()) + " cards")
-
-        logging.info("Make a new Card for: " + card_front)
-        fact = self.deck.newNote(forDeck=False)
-        fact['Front'] = card_front
-        fact['Back'] = card_back
-
-        # Add Card to the Deck
-        try:
-            self.deck.addNote(fact)
-        except:
-            if hasattr(e, "data"):
-                sys.exit("ERROR: Could not add '" + e.data['field'] + "': " +
-                         e.data['type'])
-            else:
-                sys.exit(e)
-
-        # Done.
-        logging.info("Save the Deck")
-        self.deck.save()
-        self.deck.close()
-
-    def detectModel(self,model_name):
+    def detectModel(self, model_name):
         models = self.deck.models.allNames()
         if model_name in models:
             return True
@@ -63,7 +27,7 @@ class CardManager(object):
         for item in self.deck.models.allNames():
             print("Name of model: {}".format(item))
 
-    def newModel(self,model_name):
+    def newModel(self, model_name):
         new_model = self.deck.models.new(model_name)
         # ====== copy the color blue ================
         # Change flds, tmples, css, req to other type
@@ -71,32 +35,41 @@ class CardManager(object):
         equallist = ['flds', 'tmpls', 'css', 'req']
         for item in equallist:
             new_model[item] = basic_model[item]
-        #============================================
+        # ============================================
         self.deck.models.add(new_model)
         self.deck.save()
 
-    def newCardCreate(self, model_name, content_dic):
+    def newCardCreate(self, model_name, content_dic, tags=None):
         # Please input content_list
         # with the list of the dictionary
         select_model = self.deck.models.byName(model_name)
         self.deck.models.setCurrent(select_model)
         select_model['did'] = self.deckID
         logging.info("Make a new Card ")
-        fact = self.deck.newNote(forDeck=False)
+        note = self.deck.newNote(forDeck=False)
+        if tags:
+            for t in tags:
+                note.addTag(t)
         for key, value in content_dic.items():
-            print("The content of {} is {}".format(key, content_dic[key]))
-            fact[key] = content_dic[key]
-        self.deck.addNote(fact)
+            # print("The content of {} is {}".format(key, content_dic[key]))
+            note[key] = content_dic[key]
+        self.deck.addNote(note)
         self.deck.save()
         self.deck.close()
 
+
 def test():
-    coll_file = '/Users/peihao/Library/Application Support/Anki2/個人檔案 1/collection.anki2'
-    deck_name = 'Test'
-    model_name = 'Basic'
-    content_list = {'Front': 'This is front', 'Back': 'This is back'}
+    import utils.config_loader
+    args = utils.config_loader.load_config()
+    coll_file = args.collection
+    deck_name = args.deck
+    print(coll_file, deck_name)
+
+    model_name = 'KindleJP'
+    content_list = {'word': '行く', 'stem': 'いく', 'context': 'どこに行きますか。', 'explanation': 'BlahBlah'}
     TestCard = CardManager(coll_file, deck_name)
     TestCard.newCardCreate(model_name, content_list)
+
 
 def load_config(path):
     with open(path, encoding='utf-8') as data_file:
@@ -105,15 +78,4 @@ def load_config(path):
 
 if __name__ == '__main__':
     logging.getLogger().setLevel(logging.DEBUG)
-
-    coll_file = '/Users/peihao/Library/Application Support/Anki2/個人檔案 1/collection.anki2'
-    deck_name = 'Test'
-    # TestCard = CardManager(coll_file, deck_name)
-    # TestCard.create("a","b")
-    # TestCard.test()
-    # config_path = './model/color_blue.json'
-    # data = load_config(config_path)
-    # print("Data is : {}".format(data))
-    # model_name = 'Basic'
-    # content_list = {'Front': 'This is front', 'Back' : 'This is back' }
     test()
