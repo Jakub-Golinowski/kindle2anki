233f948dd6320d9692b7062329222641c193f808
Chen Pei Hao <s894117@gmail.com>
2018-01-05 14:37:02 +0900

    Fix the type of sentence & cloze

--- cloze_process.py
+++ cloze_process.py
@@ -14,42 +14,52 @@ def mecabCheck():
         logging.warning("Without Mecab library")
         return  None
 
-def splitWord(sentance, word):
-    contents = sentance.split(word)
+def splitWord(sentence, word):
+    contents = sentence.split(word)
     prefix, suffix = contents
     print("prefix : {}".format(prefix))
     print("suffix : {}".format(suffix))
+    return prefix, suffix
 
-def replaceWord(sentance, word, mecab):
-    if word in sentance:
-        prefix, suffix = sentance.split(word)
-        output_word = word
+def replaceWord(sentence, word, mecab):
+    result = {}
+    if word in sentence:
+        content = sentence.split(word)
+        cloze_content = cloze_process(sentence, word)
     else:
         mt = mecab.Tagger("-Ochasen")
-        node = mt.parseToNode(sentance)
+        node = mt.parseToNode(sentence)
         while node:
             words = node.feature.split(',')
             if words[6] == word:
-                output_word = node.surface
-                prefix, suffix = sentance.split(output_word)
+                cloze_content = cloze_process(sentence, node.surface)
             node = node.next
+    result['Sentence'], result['Cloze'] = cloze_content
+    return result
 
-    return prefix, suffix, output_word
-
-def mecabInterface(Sentence, word):
+def mecab_interface(sentence, word):
     mecab = mecabCheck()
+    result = {}
     if mecab is not None:
-        return replaceWord(Sentence, word, mecab)
+        return replaceWord(sentence, word, mecab)
     else:
-        return Sentence, None, None
+        result['Sentence'] = sentence
+        result['Cloze'] = sentence
+        return result
+
+def highlight_word(word):
+    return '<span class=highlight>{}</span>'.format(word)
+
+def cloze_process(sentence, word):
+    content = sentence.split(word)
+    output_sentance = "{}{}{}".format(content[0], highlight_word(word), content[1])
+    output_cloze = "{}{}{}".format(content[0], highlight_word("[......]"), content[1])
+    return output_sentance, output_cloze
+
 
 if __name__ == '__main__':
     inputSentance = '土地勘のあるきみに協力して欲しいだけなのさ、三人目の、あるいは百一人目の被害者を出さないために」 　' \
             'たまには縁もゆかりもない女子を助けてみるのも乙だろう──と、臥煙さんは、僕の来歴を揶揄するようなことを言ってきた。'
-    # splitWord(inputSentance, "出さない")
-    # exit(0)
-    # print(replaceWord(inputSentance, "出す"))
-    pre, suf, word = mecabInterface(inputSentance, "出す")
-    print("The prefix is {}".format(pre))
-    print("The suffix is {}".format(suf))
-    print("The word is {}".format(word))
+    result = mecab_interface(inputSentance, "出す")
+    print("The Sentence is {}".format(result['Sentence']))
+    print("The Cloze is {}".format(result['Cloze']))
